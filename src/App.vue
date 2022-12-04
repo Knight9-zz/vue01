6720560cb82a0aa177fb9b40f1789bb45e31734d
“Knight9-zz” <1073490419@qq.com>
2022-12-04 17:23:50 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Content from './components/Content.vue';
+
+export default {
+  data(){
+    return {};
+  },
+
+  components:{
+    Content
+}
+}
+</script>
+
+<template>
+  <div>
+    <Content><el-button type="success">Success</el-button></Content>
+  </div>
+</template>
---
+++ src/components/Content.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2>Content组件</h2>
+        <slot></slot>
+    </div>
+</template>
